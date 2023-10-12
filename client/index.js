ac991f65f2c12ca08317e8125bee1404702a8f17
root <root@chw1119>
2023-10-12 21:11:29 +0900

    html canvas2 animating

--- client/index.js
+++ client/index.js
@@ -115,6 +115,65 @@ window.onload = () => {
 
     }, 1000 / 40);
 
+    const canvas1 = document.getElementById("canvas2");
+    const ctx1 = canvas1.getContext("2d");
+
+    canvas1.width = window.innerWidth;
+    canvas1.height = window.innerHeight;
+
+    const circles = [];
+
+    function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+    }
+
+    function createCircle() {
+    const radius = Math.random() * 50 + 10;
+    const x = Math.random() * (canvas.width - radius * 2) + radius;
+    const y = Math.random() * (canvas.height - radius * 2) + radius;
+    const color = getRandomColor();
+    const speedX = (Math.random() - 0.5) * 8;
+    const speedY = (Math.random() - 0.5) * 8;
+
+    circles.push({ x, y, radius, color, speedX, speedY });
+    }
+
+    function draw() {
+        ctx1.clearRect(0, 0, canvas.width, canvas.height);
+        
+        ctx1.fillStyle = "#000000";
+        ctx1.fillRect(0, 0, canvas1.width, canvas1.height);
+
+        for (let i = 0; i < circles.length; i++) {
+            const circle = circles[i];
+
+            ctx1.beginPath();
+            if (circle.radius > 0) {
+            ctx1.arc(circle.x, circle.y, circle.radius, 0, Math.PI * 2);
+            ctx1.fillStyle = circle.color;
+            ctx1.fill();
+            }
+
+            circle.x += circle.speedX;
+            circle.y += circle.speedY;
+            circle.radius -= 0.2;
+        }
+    }
+
+    function animate() {
+        
+        createCircle();
+        draw();
+        
+    }
+
+    setInterval(animate, 1000 / 40); // 40 frames per second
+
 
 
 }
@@ -132,15 +191,13 @@ function setCanvasSize() {
         
         const ctx = canvas.getContext('2d');
         
-        if (index === 0) {
-            ctx.fillStyle = 'red';
-            ctx.fillRect(0, 0, canvas.width, canvas.height);
-        } else {
             const randomColor = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
             ctx.fillStyle = randomColor;
             ctx.fillRect(0, 0, canvas.width, canvas.height);
-        }
+        
     });
+
+    
 }
 
 window.addEventListener('resize', setCanvasSize);
