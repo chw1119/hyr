4817cab1fb248e7f36cec91a977218e735d85147
USER <USER@DESKTOP-OMVI8IN>
2023-11-06 18:13:28 +0900

    os section animate

--- client/index.js
+++ client/index.js
@@ -165,6 +165,123 @@ window.onload = () => {
         }
     }
 
+        // modified version of random-normal
+    function normalPool(o){var r=0;do{var a=Math.round(normal({mean:o.mean,dev:o.dev}));if(a<o.pool.length&&a>=0)return o.pool[a];r++}while(r<100)}function randomNormal(o){if(o=Object.assign({mean:0,dev:1,pool:[]},o),Array.isArray(o.pool)&&o.pool.length>0)return normalPool(o);var r,a,n,e,l=o.mean,t=o.dev;do{r=(a=2*Math.random()-1)*a+(n=2*Math.random()-1)*n}while(r>=1);return e=a*Math.sqrt(-2*Math.log(r)/r),t*e+l}
+
+    const NUM_PARTICLES = 600;
+    const PARTICLE_SIZE = 0.5; // View heights
+    const SPEED = 20000; // Milliseconds
+
+    let particles = [];
+
+    function rand(low, high) {
+    return Math.random() * (high - low) + low;
+    }
+
+    function createParticle(canvas) {
+    const colour = {
+        r: 255,
+        g: randomNormal({ mean: 125, dev: 20 }),
+        b: 50,
+        a: rand(0, 1),
+    };
+    return {
+        x: -2,
+        y: -2,
+        diameter: Math.max(0, randomNormal({ mean: PARTICLE_SIZE, dev: PARTICLE_SIZE / 2 })),
+        duration: randomNormal({ mean: SPEED, dev: SPEED * 0.1 }),
+        amplitude: randomNormal({ mean: 16, dev: 2 }),
+        offsetY: randomNormal({ mean: 0, dev: 10 }),
+        arc: Math.PI * 2,
+        startTime: performance.now() - rand(0, SPEED),
+        colour: `rgba(${colour.r}, ${colour.g}, ${colour.b}, ${colour.a})`,
+    }
+    }
+
+    function moveParticle(particle, canvas2, time) {
+    const progress = ((time - particle.startTime) % particle.duration) / particle.duration;
+    return {
+        ...particle,
+        x: progress,
+        y: ((Math.sin(progress * particle.arc) * particle.amplitude) + particle.offsetY),
+    };
+    }
+
+    function drawParticle(particle, canvas2, ctx2) {
+    canvas2 = document.getElementById('canvas3');
+    const vh = canvas.height / 100;
+
+    ctx2.fillStyle = particle.colour;
+    ctx2.beginPath();
+    ctx2.ellipse(
+        particle.x * canvas.width,
+        particle.y * vh + (canvas.height / 2),
+        particle.diameter * vh,
+        particle.diameter * vh,
+        0,
+        0,
+        2 * Math.PI
+    );
+    ctx2.fill();
+    }
+
+    function draw1(time, canvas2, ctx2) {
+    // Move particles
+    particles.forEach((particle, index) => {
+        particles[index] = moveParticle(particle, canvas2, time);
+    })
+
+    // Clear the canvas
+    ctx2.clearRect(0, 0, canvas2.width, canvas2.height);
+    ctx2.fillStyle = "#000000";
+    ctx2.fillRect(0, 0, canvas1.width, canvas1.height);
+
+    // Draw the particles
+    particles.forEach((particle) => {
+        drawParticle(particle, canvas2, ctx2);
+    })
+
+    // Schedule next frame
+    requestAnimationFrame((time) => draw1(time, canvas2, ctx2));
+    }
+
+    function initializeCanvas() {
+    let canvas = document.getElementById('canvas3');
+    canvas2.width = canvas2.offsetWidth * window.devicePixelRatio;
+    canvas2.height = canvas2.offsetHeight * window.devicePixelRatio;
+    let ctx2 = canvas.getContext("2d");
+
+    window.addEventListener('resize', () => {
+        canvas2.width = canvas2.offsetWidth * window.devicePixelRatio;
+        canvas2.height = canvas2.offsetHeight * window.devicePixelRatio;
+        ctx2 = canvas2.getContext("2d");
+    })
+
+    return [canvas2, ctx2];
+    }
+
+    function startAnimation() {
+    const [canvas, ctx2] = initializeCanvas();
+
+    // Create a bunch of particles
+    for (let i = 0; i < NUM_PARTICLES; i++) {
+        particles.push(createParticle(canvas));
+    }
+    
+    requestAnimationFrame((time) => draw1(time, canvas, ctx2));
+    };
+
+    // Start animation when document is loaded
+    (function () {
+    if (document.readystate !== 'loading') {
+        startAnimation();
+    } else {
+        document.addEventListener('DOMContentLoaded', () => {
+        startAnimation();
+        })
+    }
+    }());
+
     function animate() {
         
         createCircle();
@@ -189,7 +306,7 @@ function setCanvasSize() {
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
         
-        const ctx = canvas.getContext('2d');
+        const ctx = canvas2.getContext('2d');
         
             const randomColor = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
             ctx.fillStyle = randomColor;
