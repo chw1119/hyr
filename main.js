5a95634ab787542ca251944a71d8e608eaf00b9e
USER <USER@DESKTOP-OMVI8IN>
2023-11-06 17:39:38 +0900

    sex

--- main.js
+++ main.js
@@ -9,4 +9,8 @@ app.get("/", function(req, res) {
     res.sendFile(__dirname + "/client/main.html");
 })
 
+app.get("/ads.txt", function(req,res){
+    res.send("google.com, pub-7170774391473826, DIRECT, f08c47fec0942fa0");
+})
+
 app.listen(80);
